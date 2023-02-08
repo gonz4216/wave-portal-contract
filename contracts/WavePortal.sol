9a813c27045b98ef336ed193c739b83ac09388e0
gonz4216 <ethgon19@gmail.com>
2023-02-07 22:58:33 -0500

    first

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+
+    constructor() {
+
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved", msg.sender);
+    }
+    function getTotalWaves() public view returns (uint256) {
+        console.log("we have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
+
+  console.log("deplying contract with account", deployer.address);
+  console.log("accounts balance: ", accountBalance.toString());
+
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+
+  console.log("wave portal address: ", waveContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+
+runMain()
